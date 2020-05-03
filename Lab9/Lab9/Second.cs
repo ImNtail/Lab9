059da11ef034ecde00d14c69b5d47d17f9586f6c
Nikita Maslov <55882052+ImNtail@users.noreply.github.com>
2020-05-03 23:16:18 +0300

    Update Second.cs

--- Lab9/Lab9/Second.cs
+++ Lab9/Lab9/Second.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace Lab9
 {
+    /*
+    С помощью стека проверить, что математическое выражение в скобках
+    записано корректно.
+    */
     public class Second
     {
         public static void Execute()
         {
+            Console.Write("Write example: ");
+            string example = Console.ReadLine();
+            string brackets = String.Empty;
+            foreach (char ch in example)
+                if (ch == '(' || ch == ')')
+                    brackets += ch;
+            Stack<char> stk = new Stack<char>();
+            foreach (char ch in brackets)
+                if (ch == '(')
+                    stk.Push(ch);
+                else if (ch == ')' && stk.Count != 0)
+                    stk.Pop();
+            if (stk.Count == 0)
+                Console.WriteLine("The example is right");
+            else
+                Console.WriteLine("The example has mistakes!");
         }
     }
 }
