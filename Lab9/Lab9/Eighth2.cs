1629cfc518bd8005bbb24c39ba4bc44d2ed79ef4
Nikita Maslov <55882052+ImNtail@users.noreply.github.com>
2020-05-15 18:51:53 +0300

    Update Eighth2.cs

--- Lab9/Lab9/Eighth2.cs
+++ Lab9/Lab9/Eighth2.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,9 +16,106 @@ namespace Lab9
         функции: заполнения и вывода матрицы, транспонирования матрицы,
         суммирования и перемножения матриц.
         */
+        public class Node<T>
+        {
+            public Node(T data)
+            {
+                Data = data;
+            }
+            public T Data { get; set; }
+            public Node<T> Next { get; set; }
+        }
+        public class LinkedList<T> : IEnumerable<T>
+        {
+            Node<T> head;
+            Node<T> tail;
+            int count;
+            public void Add(T data)
+            {
+                Node<T> node = new Node<T>(data);
+
+                if (head == null)
+                    head = node;
+                else
+                    tail.Next = node;
+                tail = node;
+                count++;
+            }
+            public bool Remove(T data)
+            {
+                Node<T> current = head;
+                Node<T> previous = null;
+
+                while (current != null)
+                {
+                    if (current.Data.Equals(data))
+                    {
+                        if (previous != null)
+                        {
+                            previous.Next = current.Next;
+                            if (current.Next == null)
+                                tail = previous;
+                        }
+                        else
+                        {
+                            head = head.Next;
+                            if (head == null)
+                                tail = null;
+                        }
+                        count--;
+                        return true;
+                    }
+                    previous = current;
+                    current = current.Next;
+                }
+                return false;
+            }
+            public int Count { get { return count; } }
+            public bool IsEmpty { get { return count == 0; } }
+            public void Clear()
+            {
+                head = null;
+                tail = null;
+                count = 0;
+            }
+            public bool Contains(T data)
+            {
+                Node<T> current = head;
+                while (current != null)
+                {
+                    if (current.Data.Equals(data))
+                        return true;
+                    current = current.Next;
+                }
+                return false;
+            }
+            public void AppendFirst(T data)
+            {
+                Node<T> node = new Node<T>(data);
+                node.Next = head;
+                head = node;
+                if (count == 0)
+                    tail = head;
+                count++;
+            }
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return ((IEnumerable)this).GetEnumerator();
+            }
+            IEnumerator<T> IEnumerable<T>.GetEnumerator()
+            {
+                Node<T> current = head;
+                while (current != null)
+                {
+                    yield return current.Data;
+                    current = current.Next;
+                }
+            }
+        }
         public static void Execute()
         {
-
+            LinkedList<int> list = new LinkedList<int>();
+            
         }
     }
 }
